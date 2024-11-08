fbebbbab8641404cff9354a2071ed0897d48be7c
teacher-mgt <min-k-t@hanmail.net>
2024-11-08 14:34:14 +0900

    20241108

---
+++ 07_inherit/src/pkg02_has_a2/Main.java
@@ -0,0 +1,14 @@
+package pkg02_has_a2;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+    Person person = new Person();
+    person.setPen(new Pen());
+    
+    person.drawing();
+
+  }
+
+}
---
+++ 07_inherit/src/pkg02_has_a2/Pen.java
@@ -0,0 +1,9 @@
+package pkg02_has_a2;
+
+public class Pen {
+
+  public void drawing() {
+    System.out.println("그린다.");
+  }
+  
+}
