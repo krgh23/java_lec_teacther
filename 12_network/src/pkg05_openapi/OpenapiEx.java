303759af88782d57abae6a7418d84e14ea515883
teacher-mgt <min-k-t@hanmail.net>
2024-11-19 14:51:15 +0900

    20241119

--- 12_network/src/pkg05_openapi/OpenapiEx.java
+++ 12_network/src/pkg05_openapi/OpenapiEx.java
@@ -8,6 +8,8 @@ import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -16,6 +18,8 @@ import org.w3c.dom.NodeList;
 public class OpenapiEx {
 
   public static void a() throws Exception {
+    
+    // 응답 결과 받기
     
     String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1%2BwT%2B5jqamBef%2FErC%2F5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ%3D%3D";
     
@@ -52,6 +56,8 @@ public class OpenapiEx {
   }
   
   public static void b() throws Exception {
+    
+    // 응답 XML 파싱하기
     
     String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1%2BwT%2B5jqamBef%2FErC%2F5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ%3D%3D";
     
@@ -92,8 +98,72 @@ public class OpenapiEx {
     
   }
   
+  public static void c() throws Exception {
+    
+    /* JSONObject 사용법 */
+    /*
+    JSONObject obj = new JSONObject("{\"name\": \"kim\", \"age\": 30}");
+    System.out.println(obj.getString("name"));
+    System.out.println(obj.getInt("age"));
+    */
+    
+    String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1%2BwT%2B5jqamBef%2FErC%2F5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ%3D%3D";
+    
+    StringBuilder builder = new StringBuilder();
+    builder.append("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst");
+    builder.append("?serviceKey=").append(serviceKey);
+    builder.append("&pageNo=").append(1);
+    builder.append("&numOfRows=").append(1000);
+    builder.append("&base_date=").append(20241118);
+    builder.append("&base_time=").append(0600);
+    builder.append("&nx=").append(61);
+    builder.append("&ny=").append(125);
+    builder.append("&dataType=").append("JSON");
+    
+    URL url = new URL(builder.toString());
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    
+    BufferedReader in = null;
+    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+      in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    } else {
+      in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+    }
+    
+    StringBuilder responseBody = new StringBuilder();
+    String line = null;
+    while ((line = in.readLine()) != null)
+      responseBody.append(line);
+    
+    System.out.println(responseBody.toString());
+    
+    in.close();
+    conn.disconnect();
+    
+    
+    JSONArray item = new JSONObject(responseBody.toString())
+                          .getJSONObject("response")
+                          .getJSONObject("body")
+                          .getJSONObject("items")
+                          .getJSONArray("item");
+    // System.out.println(item.length());
+    // System.out.println(item.getJSONObject(0).getString("category"));
+    for(int i = 0, length = item.length(); i < length; i++) {
+      System.out.println(item.getJSONObject(i).getString("baseDate"));
+      System.out.println(item.getJSONObject(i).getString("baseTime"));
+      System.out.println(item.getJSONObject(i).getString("category"));
+      System.out.println(item.getJSONObject(i).getString("fcstDate"));
+      System.out.println(item.getJSONObject(i).getString("fcstTime"));
+      System.out.println(item.getJSONObject(i).getString("fcstValue"));
+      System.out.println(item.getJSONObject(i).getInt("nx"));
+      System.out.println(item.getJSONObject(i).getInt("ny"));
+      System.out.println("--------------");
+    }
+    
+  }
+  
   public static void main(String[] args) throws Exception {
-    b();   
+    c();
   }
 
 }
