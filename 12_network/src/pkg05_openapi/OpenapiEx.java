5e7ed1c231ea89cf7c3cdf1dcfd2ad07db950913
teacher-mgt <min-k-t@hanmail.net>
2024-11-19 12:11:10 +0900

    20241119

---
+++ 12_network/src/pkg05_openapi/OpenapiEx.java
@@ -0,0 +1,43 @@
+package pkg05_openapi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenapiEx {
+
+  public static void main(String[] args) throws Exception {
+    
+    String serviceKey = "bEQBRPHjt0tZrc7EsL0T8usfsZ1%2BwT%2B5jqamBef%2FErC%2F5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ%3D%3D";
+    
+    StringBuilder builder = new StringBuilder();
+    builder.append("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst");
+    builder.append("?serviceKey=").append(serviceKey);
+    builder.append("&pageNo=").append(1);
+    builder.append("&numOfRows=").append(1000);
+    builder.append("&base_date=").append(20241118);
+    builder.append("&base_time=").append(0600);
+    builder.append("&nx=").append(61);
+    builder.append("&ny=").append(125);
+    
+    URL url = new URL(builder.toString());
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    
+    BufferedReader in = null;
+    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+      in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    } else {
+      in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+    }
+    
+    StringBuilder responseBody = new StringBuilder();
+    String line = null;
+    while ((line = in.readLine()) != null)
+      responseBody.append(line);
+    
+    System.out.println(responseBody.toString());
+
+  }
+
+}
